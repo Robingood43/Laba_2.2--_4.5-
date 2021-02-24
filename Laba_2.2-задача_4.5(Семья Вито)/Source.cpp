e0b3041c3a8f215f8dea0dd77a9c9931dfa361a9
Kirill Sharapov <62686901+Robingood43@users.noreply.github.com>
2021-02-24 23:00:08 +0600

    Add files via upload

---
+++ Laba_2.2-задача_4.5(Семья Вито)/Source.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, r, s[505];
+	while (~scanf_s("%d", &n))
+	{
+		while (n--) {
+			scanf_s("%d", &r);
+			for (int i = 0; i < r; ++i)
+				scanf_s("%d", &s[i]);
+			sort(s, s + r);
+			int sum = 0;
+			for (int i = 0; i < r; ++i)
+				sum += abs(s[i] - s[r >> 1]);
+			printf("%d\n", sum);
+		}
+	}
+	return 0;
+}
